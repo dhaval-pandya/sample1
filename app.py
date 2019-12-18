1a0d4276d6c6282ccbbcc0d04745b9a9677e0c51
dhaval-pandya <mailtodhaval@gmail.com>
2019-12-19 00:05:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import sys
+
+ 
+
+def process_input(a, b, operation):
+
+   “””Perform operation on *a* and *b* depending on input provided.
+
+   :param a: integer value
+
+   :param b: integer value
+
+   “””
+
+   if operation == “add”:
+
+       return a + b
+
+   if operation == “subtract”:
+
+       return a – b
+
+   if operation == “multiple”:
+
+       return a * b
+
+   if operation == “divide”:
+
+       if b == 0:
+
+           return “Invalid Input”
+
+       return a / b
+
+ 
+
+if __name__ == “__main__”:
