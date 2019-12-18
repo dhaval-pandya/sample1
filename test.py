aeb3d8b0dbb8edcea82321a40e59fb94939558ab
dhaval-pandya <mailtodhaval@gmail.com>
2019-12-19 00:07:15 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from app import process_input
+
+ 
+
+class TestApp(unittest.TestCase):
+
+   “””Test the mathematical operations app
+
+   “””
+
+   def setUp(self):
+
+       “This runs before the test cases are executed”
+
+       self.a = 10
+
+       self.b = 5
+
+   def test_0010_add(self):
+
+       “Test add operation”
+
+       result = process_input(self.a, self.b, “add”)
+
+       self.assertEqual(result, 15)
+
+ 
+
+def suite():
+
+   “Test suite”
+
+   suite = unittest.TestSuite()
+
+   suite.addTests(
+
+       unittest.TestLoader().loadTestsFromTestCase(TestApp)
+
+   )
+
+   return suite
+
+ 
+
+if __name__ == ‘__main__’:
+
+   unittest.TextTestRunner(verbosity=2).run(suite())
